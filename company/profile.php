052131a067e9f5dad416ef40bd603908b451a9bf
alfonsiaronna <alfonsiaronnalf@gmail.com>
2016-12-10 20:04:46 +0700

    ..

--- company/profile.php
+++ company/profile.php
@@ -1,22 +1,22 @@
 <?php
 	//menghindari direct access header,footer,db,dll
 	define('Access', TRUE);
-	require_once('header.php');
+	require_once( __DIR__.'/../header.php');
 	
 	if(strlen($_SESSION["current"]) == 0){
-		if(!isset($GET["id"])){
+		if(!isset($_GET["id"])){
 			header("location:http://". getFolderUrl() ."error.php");
 		}
 		else{
-			require_once("profile/profile.php");
+			require_once("profile.php");
 		}
 	}
 	else{
 		if(!isset($_GET["id"])){
-			require_once("profile/myprofile.php");
+			require_once("company/myprofile.php");
 		}
 		else{
-			require_once("profile/profile.php");
+			require_once("company/profile.php");
 		}
 	}
 	
---
+++ logincompany.php
@@ -0,0 +1,62 @@
+<?php
+	define('Access', TRUE);
+	require_once("header.php");
+	$errors = array();
+	if(isset($_POST["signin"])){
+		$username = $_POST["inputUsername"];
+		$password = $_POST["inputPassword"];
+		$db->where('username', $username);
+		$results = $db->getOne('company');
+		if(empty($results)){
+			array_push($errors, "Username doesn't exist");
+		}
+		else if(password_verify($password, $results['password'])){
+			echo "<script> alert 'masuk'; </script>";
+			if($results["lost_password_request"] != "0"){
+				array_push($errors, "Account had requested reset password, Check your email.");
+			}
+			else{
+				$_SESSION["current"] = $results['id'];
+				$_SESSION["role"] = $results['role'];
+				
+				$data = Array ('last_sign_in_stamp' => date("Y-m-d H:i:s"));
+				$db->where ('id', $_SESSION["current"]);
+				$db->update ('company', $data);
+				header("location:index.php");
+			}
+		}
+		else{
+			array_push($errors, "Wrong password");
+		}
+	}
+?>
+	<div class="wrapper">
+		<div class="container">
+			<form class="form-signin" method="post" action="http://<?php echo getFolderUrl();?>logincompany.php">
+				<h2 class="form-signin-heading">Sign in</h2>
+				<label for="inputUsername" class="sr-only">Username</label>
+				<input type="text" id="inputUsername" class="form-control" placeholder="Username" name="inputUsername" required autofocus>
+				<label for="inputPassword" class="sr-only">Password</label>
+				<input type="password" id="inputPassword" class="form-control" placeholder="Password" name="inputPassword" required>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" value="remember-me"> Remember me
+					</label>
+				</div>
+				<button class="btn btn-lg btn-primary btn-block" type="submit" name="signin">Sign in</button>
+				<a href="http://<?php echo getFolderUrl();?>passwordreset.php"><button class="btn btn-lg btn-danger btn-block">Forgot password?</button></a>
+				<?php
+					foreach($errors as $error){
+						echo "<br>";
+						echo '<div class="alert alert-danger">
+								<strong>Error!</strong> '. $error .'.
+							</div>';
+					}
+				?>
+			</form>
+		</div>
+	</div>
+	
+<?php
+	require_once("footer.php");
+?>
