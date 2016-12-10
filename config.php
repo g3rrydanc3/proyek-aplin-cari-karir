f90346d2ea59371f63c9ec3d78f01d95f2f50698
Gerry Surya <ger_26_6@hotmail.com>
2016-12-10 22:17:38 +0700

    Config update

--- config.php
+++ config.php
@@ -4,23 +4,19 @@
 	}
 	
 	///////////////////CONFIG///////////////////
-	$db_server = "localhost";
-	$db_username = "root";
-	$db_password = "";
-	$db_dbname = "job_comer";
+	DEFINE('DB_SERVER', 'localhost');
+	DEFINE('DB_USER', 'root');
+	DEFINE('DB_PASSWORD', '');
+	DEFINE('DB_DBNAME', 'job_comer');
 	
-	function getFolderWebsite(){
-		$config_folder = "proyek-aplin-job-comer";
-		return $config_folder;
-	}
+	DEFINE('CONFIG_FOLDER', 'proyek-aplin-job-comer/');
 	
 	date_default_timezone_set('Asia/Jakarta');
 	///////////////////END OF CONFIG///////////////////
 	
-
 	require_once("pages/MysqliDb.php");
 	
-	$db = new MysqliDb ($db_server, $db_username, $db_password, $db_dbname);
+	$db = new MysqliDb (DB_SERVER, DB_USER, DB_PASSWORD, DB_DBNAME);
 	$db->ping();
 	
 	session_start();
@@ -68,7 +64,7 @@
 	
 	function getFolderUrl(){
 		$url = $_SERVER['REQUEST_URI'];
-		$dir = $_SERVER['SERVER_NAME'] . "/" . getFolderWebsite() . "/";
+		$dir = $_SERVER['SERVER_NAME'] . "/" . CONFIG_FOLDER;
 		return $dir;
 	}
 	
