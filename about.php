efc062f91003a84cea5ea7b5232e492e4daf9de7
Andhika21 <andhikakurnia21@gmail.com>
2016-12-13 12:41:34 +0700

    about us, contact us

--- about.php
+++ about.php
@@ -5,7 +5,12 @@
 ?>
 	<div class="wrapper">
 		<div class="container">
-			About Us
+			<div class="jumbotron">
+				<h1>About Us</h1>
+				<p class>Job Comer adalah salah satu web untuk mencari pekerjaan yang ditawarkan berbagai perusahaan yang ada di Indonesia.</p>
+				<p>Tidak hanya itu, Job Comer juga dapat memberi fasilitas kepada perusahaan yang mencari pekerja yang sesuai dengan apa yang di butuhkan perusahaan tersebut.</p>
+				<p>Dengan kata lain Job Comer membantu anda dalam proses mencari pekerjaan yang sesuai dengan kemampuan anda.</p>
+			</div>
 		</div>
 	</div>
 <?php
